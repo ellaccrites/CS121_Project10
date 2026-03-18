d3fa22a16692f4fc1f89bc7e5e96e2aa7e242f77
ellaccrites <ellac.crites2006@gmail.com>
2026-03-18 17:43:59 +0000

    started Customer, fixed SavingsAccount

---
+++ Customer.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Customer extends User implements HasMenu {
+	CheckingAccount checking;
+	SavingsAccount savings;
+
+	public static void main(String[] args){
+		Customer c = new Customer();
+		c.start();
+	}// end main
+	
+	public Customer(){
+		
+	}// end customer
+	
+	public Customer(String userName
+
+
+
+}// end class
