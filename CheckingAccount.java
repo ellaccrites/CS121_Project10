37e45bbc6cf214ea86f477681030c55c11f46e1e
ellaccrites <ellac.crites2006@gmail.com>
2026-03-18 04:22:58 +0000

    added README, makefile, HasMenu, and Checking account

---
+++ CheckingAccount.java
@@ -0,0 +1,108 @@
+import java.util.*;
+import java.io.*;
+
+public class CheckingAccount implements HasMenu{
+	double balance;
+
+	public static void main(String[] args){
+		CheckingAccount ca = new CheckingAccount();
+		ca.start();
+	}// end main
+
+	public CheckingAccount(){
+		this.balance = 0d;
+	}// end no param constructor
+	
+	public CheckingAccount(double balance){
+		this.balance = balance;
+	}// end constructor
+	
+	public String menu(){
+		System.out.println("0) quit");
+		System.out.println("1) check balance");
+		System.out.println("2) make a deposit");
+		System.out.println("3) make a withdrawal");
+		System.out.print("Choose 0-3: ");
+
+		Scanner input = new Scanner(System.in);
+		String userChoice = input.nextLine();
+
+		return userChoice;
+	}// end menu
+	
+	public void start(){
+		boolean keepGoing = true;
+		while(keepGoing){
+			String userChoice = this.menu();
+			if(userChoice.equals("0")){
+				System.out.println("Leaving Account Menu...");
+				keepGoing = false;
+			} else if(userChoice.equals("1")){
+				System.out.println("Checking balance...");
+				this.checkBalance();
+			} else if(userChoice.equals("2")){
+				System.out.println("Making a deposit...");
+				this.makeDeposit();
+			} else if(userChoice.equals("3")){
+				System.out.println("Making a withdrawal...");
+				this.makeWithdrawal();
+			} else{
+				System.out.println("Not a valid input.");
+			}// end else if
+			System.out.println();
+		}// end while
+	}// end start
+	
+	public double getBalance(){
+		return this.balance;
+	}// end getBalance
+	
+	public String getBalanceString(){
+		String result = String.format("$%.02f", this.balance);
+		return result;
+	}// end get balance string
+	
+	public void setBalance(double balance){
+		this.balance = balance;
+	}// end setBalance
+	
+	public void checkBalance(){
+		System.out.println("Current balance: " + this.getBalanceString());
+	}// end checkBalance
+	
+	private double getDouble(){
+		Scanner input = new Scanner(System.in);
+		String amount = input.nextLine();
+		
+		double doubleAmount = 0d;
+		try{
+			doubleAmount = Double.parseDouble(amount);
+		}catch(NumberFormatException e){
+			System.out.println("Please input a number.");
+		}// end try
+		return doubleAmount;
+	}// end getDouble
+	
+	public void makeDeposit(){
+		System.out.print("Amount to deposit? ");
+		double newBalance = this.balance + getDouble();
+		this.setBalance(newBalance);
+
+		System.out.println("New balance: " + getBalanceString());
+	}// end makeDeposit
+	
+	public void makeWithdrawal(){
+                System.out.print("Amount to withdrawal? ");
+		double toWithdraw = 0d;
+		toWithdraw = getDouble();
+
+		if(toWithdraw > this.balance){
+			System.out.println("There is not enough in account to withdraw entered amount.");
+		}else{
+			double newBalance = this.balance - toWithdraw;
+			this.setBalance(newBalance);
+			System.out.println("New balance: " + getBalanceString());
+		}// end if else
+	}// end makeWithdrawal
+
+}// end class
